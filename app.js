0a2abb8906cacdbde522de4cfee5bf9f323ad85f
akshayamin62 <akshay.amin.62@gmail.com>
2024-12-22 22:08:54 +0530

    Delete Review Feature implemented

--- app.js
+++ app.js
@@ -196,7 +196,7 @@ app.delete("/listings/:id/", wrapAsync(async (req,res)=>{
     res.redirect("/listings");
 }));
 
-//Reviews
+//Create Reviews
 app.post("/listings/:id/reviews", validateReview, wrapAsync(async(req,res) => {
     let listing = await Listing.findById(req.params.id);
     let newReview = new Review(req.body.review);
@@ -208,6 +208,14 @@ app.post("/listings/:id/reviews", validateReview, wrapAsync(async(req,res) => {
 
     // res.send("Review Saved");
     res.redirect(`/listings/${listing._id}`)
+}));
+
+//Delete Review
+app.delete("/listings/:id/reviews/:reviewId", wrapAsync(async(req,res)=> {
+    let {id,reviewId} = req.params;
+    await Listing.findByIdAndUpdate(id,{$pull: {reviews: reviewId}});
+    await Review.findByIdAndDelete(reviewId);
+    res.redirect(`/listings/${id}`)
 }));
 
 app.all("*",(req,res,next)=>{
