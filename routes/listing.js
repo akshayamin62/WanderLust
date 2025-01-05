b401cbce0d301f4be0d4416a201e2243316fb6c6
akshayamin62 <akshay.amin.62@gmail.com>
2025-01-05 12:48:27 +0530

    Files store at cloudinary

--- routes/listing.js
+++ routes/listing.js
@@ -7,7 +7,8 @@ const Listing = require("../models/listing.js");
 const {isLoggedIn, isOwner,validateListing} = require("../middleware.js");
 const listingController = require("../controllers/listing.js");
 const multer = require("multer");
-const upload = multer({dest: "upload/"});
+const {storage} = require("../cloudConfig.js");
+const upload = multer({storage});
 
 // Index and Create route combine 
 router.route("/")
